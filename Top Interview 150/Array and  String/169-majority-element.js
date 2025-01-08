f8a74d4bae2f08af7ca4eac0ea56aa2e8f32382b
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2025-01-08 15:40:35 +0200

    Solutuion for task - 169-majority-element

---
+++ Top Interview 150/Array and  String/169-majority-element.js
@@ -0,0 +1,43 @@
+/**
+ * 
+ * Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+Example 1:
+Input: nums = [3,2,3]
+Output: 3
+
+Example 2:
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+
+Constraints:
+n == nums.length
+1 <= n <= 5 * 104
+-109 <= nums[i] <= 109 
+
+Follow-up: Could you solve the problem in linear time and in O(1) space?
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let majorityCount = nums.length / 2;
+    let maxCountElement = 0;
+    let elementsCounts = {}; // change this to something more efficient - object
+
+    for (let i = 0; i < nums.length; i++) {
+        let element = nums[i];
+        elementsCounts[element] = (elementsCounts[element] || 0) + 1; 
+    }
+
+    for (let element in elementsCounts) {
+        if (elementsCounts[element] > majorityCount) {
+            maxCountElement = Number(element);
+        }
+    }
+
+    return maxCountElement;
+};
+
+console.log(majorityElement([2,2,1,1,1,2,2]));

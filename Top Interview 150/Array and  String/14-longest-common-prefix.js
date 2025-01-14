8c5e480e717eae1532bfe616c6addb6d2a33ef29
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2025-01-14 20:21:06 +0200

    Solution for - 14-longest-common-prefix

---
+++ Top Interview 150/Array and  String/14-longest-common-prefix.js
@@ -0,0 +1,39 @@
+/**
+ * 
+ * Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+
+Example 1:
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Example 2:
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+Constraints:
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters if it is non-empty.
+ * 
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+    if (strs.length === 0) return "";
+    let prefix = strs[0];
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) { // "flower".indexOf("fl") = 0
+            prefix = prefix.substring(0, prefix.length - 1);
+            if (prefix === "") return "";
+        }
+    }
+
+    return prefix;	
+};
+
+
+// Does this algorithm have a name? - Horizontal scanning
+// Youtube: https://www.youtube.com/watch?v=bl8ue-dTxgs
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
